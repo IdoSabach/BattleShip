e4874bcb02664b3e9d56f08d6453d00e5532fc5f
Ido <Idosabach11@gmail.com>
2023-12-26 15:33:55 +0200

    fix semistandard

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'), 
-    filename: 'main.js', 
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/dist/'
   },
   module: {
     rules: [
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource', 
-      },
-    ],
+        type: 'asset/resource'
+      }
+    ]
   },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
         { from: 'src/icons8-boom-96.png', to: 'icons8-boom-96.png' },
-        { from: 'src/102810__redbulldog98__boom.wav', to: '102810__redbulldog98__boom.wav' },
-      ],
-    }),
-  ],
-};
+        { from: 'src/102810__redbulldog98__boom.wav', to: '102810__redbulldog98__boom.wav' }
+      ]
+    })
+  ]
+}
